28f3e45cd4c2f7eff31925560513dfa2c743ea38
Shane Oh <ohshane71@gmail.com>
2021-09-29 23:39:53 +0900

    postgresql on docker compose

--- backend/visionmanager/asgi.py
+++ backend/visionmanager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lookup project.
+ASGI config for visionmanager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lookup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visionmanager.settings')
 
 application = get_asgi_application()
