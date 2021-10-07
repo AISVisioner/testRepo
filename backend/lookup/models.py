76eb4a8fd753e7b30bae1361848fd689caf3451d
Shane Oh <ohshane71@gmail.com>
2021-10-07 14:52:09 +0900

    added model

--- backend/lookup/models.py
+++ backend/lookup/models.py
@@ -1,3 +1,12 @@
+import uuid
+# from django.conf import settings
 from django.db import models
+# from django.utils import timezone
+# from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # encoding = ArrayField(models.FloatField)
+    photo = models.ImageField(blank=True, editable=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    recent_access_at = models.DateTimeField(auto_now=True, editable=False)
